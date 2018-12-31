7c19eded3dfdba73fcda8f778d39921db0256517
pepe <github@pe82.de>
2018-12-31 01:06:03 +0100

    Major redesign

--- conanfile.py
+++ conanfile.py
@@ -1,66 +1,55 @@
 import os
-import platform
-# from conans.client.generators.cmake import DepsCppCmake
 from conans.client.generators.cmake_paths import CMakePathsGenerator
-# from conans.model import Generator
+from conans.client.build.cmake_flags import CMakeDefinitionsBuilder, get_generator
 from conans import ConanFile
+from conans.util.env_reader import get_env
+from conans.model.conan_file import get_env_context_manager
 
 
-def _cmake_escape(inp: str):
-    """
-
-    :type inp: str
-    """
+def _cmake_escape_backslash(inp: str):
     return inp.replace("\\", "\\\\")
 
 
 class CmakeToolchain(CMakePathsGenerator):
-    append_with_spaces = ["CPPFLAGS", "CFLAGS", "CXXFLAGS", "LIBS", "LDFLAGS", "CL"]
-
     def _get_cmake_environment_setters(self):
-        ret = []
-        for name, value in self.conanfile.env.items():
-            if isinstance(value, list):
-                if name in self.append_with_spaces:
-                    # Variables joined with spaces look like: CPPFLAGS="one two three"
-                    value = _cmake_escape(" ".join(value))
-                    ret.append("set(ENV{{{name}}} \"{value} $ENV{{{name}}}\")".format(name=name, value=value))
-                else:
-                    if platform.system() == "Windows":
-                        value = os.pathsep.join(value)
-                    else:
-                        value = os.pathsep.join(['"%s"' % val for val in value])
-                    value = _cmake_escape(value)
-                    ret.append("set(ENV{{{name}}} \"{value};$ENV{{{name}}}\")".format(name=name, value=value))
-            else:
-                value = _cmake_escape(value)
+        old_env = dict(os.environ)
+        with get_env_context_manager(self.conanfile):
+            ret = []
+            for name, value in self.conanfile.env.items():
+                if isinstance(value, list):
+                    value = str(get_env(name)).replace(old_env[name], "$ENV{{{name}}}".format(name=name))
+                value = _cmake_escape_backslash(value)
                 ret.append("set(ENV{{{name}}} \"{value}\")".format(name=name, value=value))
-        return ret
+            return ret
 
-    def _get_cmake_toolchain_setter(self):
-        ret = []
-        if "CONAN_CMAKE_TOOLCHAIN_FILE" in self.conanfile.env:
-            toolchain = _cmake_escape(self.conanfile.env["CONAN_CMAKE_TOOLCHAIN_FILE"])
-            ret.append("set(CMAKE_TOOLCHAIN_FILE \"{toolchain}\")"
-                       .format(toolchain=toolchain))
-        return ret
+    def _get_cmake_definitions(self):
+        with get_env_context_manager(self.conanfile):
+            ret = []
+            build_type = self.conanfile.settings.get_safe("build_type")
+            generator = get_generator(self.conanfile.settings)
+            def_builder = CMakeDefinitionsBuilder(self.conanfile, generator=generator, forced_build_type=build_type)
+            definitions = def_builder.get_definitions()
+            del definitions["CONAN_EXPORTED"]
+            print(definitions)
+            for name, value in definitions.items():
+                ret.append("set({name} {value})".format(name=name, value=value))
+            return ret
 
-    # noinspection PyPropertyDefinition
     @property
     def filename(self):
-        pass
+        return "conan_toolchain.cmake"
 
     @property
     def content(self):
-        toolchain_setter = self._get_cmake_toolchain_setter()
-        env_setters = self._get_cmake_environment_setters()
-
-        return {super().filename: super().content,
-                "conan_toolchain.cmake": os.linesep.join(env_setters + toolchain_setter)}
+        lines=["if(NOT CONAN_EXPORTED)"]
+        lines.extend(self._get_cmake_environment_setters())
+        lines.extend(self._get_cmake_definitions())
+        lines.append("endif()")
+        return super().content + os.linesep.join(lines) + "\n"
 
 
 class CmakeToolchainGeneratorPackage(ConanFile):
-    name = 'CmakeToolchainGenerator'
+    name = 'cmake_toolchain_generator'
     version = '0.1'
     description = "A generator for conan that can be used to build conan packages " \
                   "by invoking cmake instead of conan build"
---
+++ test_package/conanfile.py
@@ -0,0 +1,30 @@
+import os
+
+from conans import ConanFile, CMake, tools
+from conans.util.env_reader import get_env
+
+
+class HelloTestConan(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CmakeToolchain"
+
+    def build(self):
+        self.run("cmake ../..")
+        self.run("cmake --build .")
+        #cmake = CMake(self)
+        # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is
+        # in "test_package"
+        #cmake.configure()
+        #cmake.build()
+
+    def imports(self):
+        pass
+        #self.copy("*.dll", dst="bin", src="bin")
+        #self.copy("*.dylib*", dst="bin", src="lib")
+        #self.copy('*.so*', dst='bin', src='lib')
+
+    def test(self):
+        pass
+        #if not tools.cross_building(self.settings):
+        #    os.chdir("bin")
+        #    self.run(".%sexample" % os.sep)
