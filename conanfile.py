86058e8fd39f4767d495105e71e93f877c55aa10
pepe <35840364+pepe82sh@users.noreply.github.com>
2019-08-16 12:36:30 +0200

    Changed base class and output
    For this to work correctly we need to split the output into a toolchain part that has to be included prior to the project macro, and the rest that should be included after the project macro.

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,16 @@
 import os
-from conans.client.generators.cmake_paths import CMakePathsGenerator
+from conans.client.generators.cmake import CMakeGenerator
 from conans.client.build.cmake_flags import CMakeDefinitionsBuilder, get_generator
 from conans import ConanFile
 from conans.util.env_reader import get_env
 from conans.model.conan_file import get_env_context_manager
+
+conan_toolchain_content = """
+if(NOT CONAN_EXPORTED)
+{environment}
+{definitions}
+endif()
+"""
 
 
 def _cmake_escape_backslash(inp: str):
@@ -15,17 +22,16 @@ def _cmake_escape_backslash(inp: str):
     return inp.replace("\\", "\\\\")
 
 
-class CmakeToolchain(CMakePathsGenerator):
+class CmakeToolchain(CMakeGenerator):
     """
-    conan generator for cmake toolchains. Generates a file conaining all definitions and environment variables
-    that would usually be set by conan when invoking cmake. Additionally the functionality of the built in
-    cmake_paths generator is kept.
+    conan generator for cmake toolchains. Extends conans cmake generator and creates an additional file containing
+    the toolchain and environment. That file must be included prior to the project macro in the CMake file.
     """
 
     def _get_cmake_environment_setters(self):
         """
         Detect all environment changes made by conan and convert them to cmake commands
-        :return: List of lines to print into a cmake file
+        :return: string to print into a cmake file
         """
         old_env = dict(os.environ)
         with get_env_context_manager(self.conanfile):
@@ -35,13 +41,13 @@ class CmakeToolchain(CMakePathsGenerator):
                     value = str(get_env(name)).replace(old_env[name], "$ENV{{{name}}}".format(name=name))
                 value = _cmake_escape_backslash(value)
                 ret.append("set(ENV{{{name}}} \"{value}\")".format(name=name, value=value))
-            return ret
+            return os.linesep.join(ret)
 
     def _get_cmake_definitions(self):
         """
         Detect all definitions conan sends to cmake by command line and convert them to cmake commands. Removes the
         CONAN_EXPORTED definition, so that users may check if conan has been invoked or not.
-        :return: List of lines to print into a cmake file
+        :return: string to print into a cmake file
         """
         with get_env_context_manager(self.conanfile):
             ret = []
@@ -51,28 +57,27 @@ class CmakeToolchain(CMakePathsGenerator):
             definitions = def_builder.get_definitions()
             del definitions["CONAN_EXPORTED"]
             for name, value in definitions.items():
+                value = _cmake_escape_backslash(value)
                 ret.append("set({name} {value})".format(name=name, value=value))
-            return ret
+            return os.linesep.join(ret)
 
     @property
     def filename(self):
-        """ Name of the output file """
-        return "conan_toolchain.cmake"
+        pass
 
     @property
     def content(self):
         """ Content of the output file. """
         # environment and definitions will only be set if cmake has not been invoked by conan
-        lines=["if(NOT CONAN_EXPORTED)"]
-        lines.extend(self._get_cmake_environment_setters())
-        lines.extend(self._get_cmake_definitions())
-        lines.append("endif()")
-        return super().content + os.linesep.join(lines) + "\n"
+        content = conan_toolchain_content.format(environment=self._get_cmake_environment_setters(),
+                                                 definitions=self._get_cmake_definitions())
+        return {super().filename: super().content,
+                "conan_toolchain.cmake": content}
 
 
 class CmakeToolchainGeneratorPackage(ConanFile):
     name = 'cmake_toolchain_generator'
-    version = '0.1'
+    version = '0.2'
     description = "A generator for conan that can be used to build conan packages " \
                   "by invoking cmake instead of conan build"
     url = 'https://github.com/pepe82sh/ConanCmakeToolchainGenerator'
